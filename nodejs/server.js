c5b083f2aaf658c8fb98a558a529b802e62f029a
waseem akhter <waseemakhter028@gmail.com>
2023-09-18 21:31:58 +0530

    eslint setup node js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,71 +1,60 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const errorHandler  = require('./middlewares/error-handler.js')
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const errorHandler = require('./middlewares/error-handler.js')
 
-const app = express();
+const app = express()
 const Routes = require('./routes')
 const cors = require('cors')
-const swaggerUi  = require("swagger-ui-express")
+const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json')
 
-global.appRoot = path.join(__dirname);
+global.appRoot = path.join(__dirname)
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-app.use(bodyParser.json({ limit: '2mb' }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '2mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
 app.options('*', cors())
 // Swagger Docs
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // Api Routes
-app.use('/', Routes);
+app.use('/', Routes)
 
-app.get('/', (req, res, next) => {
-    res.send('working server');
+app.get('/', (req, res) => {
+  res.send('working server')
 })
-
-
 
 // Error Middlewares
 //404 not found handle
 app.use(errorHandler.notFound)
 // generic Error handler
-app.use(errorHandler.genericErrorHandler);
-
+app.use(errorHandler.genericErrorHandler)
 
 //server configuration
 const server = app.listen(process.env.PORT, async () => {
-    console.log(`Server up successfully - host: ${process.env.HOST} port: ${process.env.PORT}`);
-});
+  console.log(`Server up successfully - host: ${process.env.HOST} port: ${process.env.PORT}`)
+})
 
 process.on('unhandledRejection', (err) => {
-  console.log('possibly unhandled rejection happened');
-  console.log(err.message);
-});
+  console.log('possibly unhandled rejection happened')
+  console.log(err.message)
+})
 
 const closeHandler = () => {
-  Object
-      .values(connections)
-      .forEach((connection) => connection.close());
+  // eslint-disable-next-line no-undef
+  Object.values(connections).forEach((connection) => connection.close())
 
   server.close(() => {
-      console.log('Server is stopped succesfully');
-      process.exit(0); /* eslint-disable-line */
-  });
-};
+    console.log('Server is stopped succesfully')
+    process.exit(0) /* eslint-disable-line */
+  })
+}
 
-process.on('SIGTERM', closeHandler);
-process.on('SIGINT', closeHandler);
-
-
-
-
-
-
+process.on('SIGTERM', closeHandler)
+process.on('SIGINT', closeHandler)
