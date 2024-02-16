4810ee7ba406d033e79981fb572305e33b451724
waseem akhter <waseemakhter028@gmail.com>
2024-02-16 15:29:05 +0530

    Mysql setup with Aiven

--- nodejs/config/config.js
+++ nodejs/config/config.js
@@ -1,34 +1,38 @@
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_POOL_MIN, DB_POOL_MAX, DB_POOL_IDLE } = process.env
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_POOL_MIN, DB_POOL_MAX, DB_POOL_IDLE, DB_PORT } =
+  process.env
 const mysql2 = require('mysql2')
 const min = parseInt(DB_POOL_MIN)
 const max = parseInt(DB_POOL_MAX)
 const idle = parseInt(DB_POOL_IDLE)
-module.exports={
-  "development": {
-    "username": DB_USER,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "mysql",
-    "dialectModule": mysql2,
-    "pool": { "max": max, "min": min, "idle": idle }
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    pool: { max: max, min: min, idle: idle }
   },
-  "test": {
-    "username": DB_USER,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "mysql",
-    "dialectModule": mysql2,
-    "pool": { "max": max, "min": min, "idle": idle }
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    pool: { max: max, min: min, idle: idle }
   },
-  "production": {
-    "username": DB_USER,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
-    "dialect": "mysql",
-    "dialectModule": mysql2,
-    "pool": { "max": max, "min": min, "idle": idle }
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    pool: { max: max, min: min, idle: idle }
   }
 }
